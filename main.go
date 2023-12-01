3b1b53066187ecb79162ef7319fd99e20eec6d35
yury <yurystarkov@users.noreply.github.com>
2023-12-01 19:31:13 +0700

    upd

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
     fs := http.FileServer(http.Dir("assets/"))
     http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(os.Getenv("PORT"), nil)
 }
 
 func sendMail(message []byte)  {
