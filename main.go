d22b0eb14eee5782a6f29f2fc1383903e02158ac
yury <yurystarkov@users.noreply.github.com>
2023-12-08 21:22:06 +0700

    upd

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type CustomerInfo struct {
 }
 
 func main() {
-    tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+    tmpl := template.Must(template.ParseFiles("./templates/index.html", "./templates/_navbar.html"))
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         if r.Method != http.MethodPost {
